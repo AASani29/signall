f20c7901a537f21d386f23c071a0231e955fd66d
Sani <alfey2001sani@gmail.com>
2024-10-13 20:17:36 +0600

    Landing Page and Login Page

---
+++ src/app/LandingPage/page.tsx
@@ -0,0 +1,11 @@
+import Header from "../Components/Header";
+import Hero from "../Components/Hero";
+
+export default function landingpage() {
+  return (
+    <main>
+      <Header />
+      <Hero />
+    </main>
+  );
+}
